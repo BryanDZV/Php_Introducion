d52ac65ccb280da0a00aa052c2c1e059fc3a8c89
BryanDZV <bryan.dweb@gmail.com>
2025-09-22 11:44:10 +0200

    clase

---
+++ ejecicios/ej3/e3/ej3.php
@@ -0,0 +1,8 @@
+<?php
+/*Realizar un pequeño programa en el que nos pida un DNI y nos visualiza la letra correspondiente.
+Tres versiones:
+1)	Href
+2)	Un formulario en un html o php y un php que lo procesa
+3)	Un único php que visualiza el formulario y lo procesa
+*/
+?>
