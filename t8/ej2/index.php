56d44bec9b69a2d60b76f98063c7201f69f79464
BryanDZV <bryan.dweb@gmail.com>
2025-10-09 16:45:09 +0200

    c

---
+++ t8/ej2/index.php
@@ -0,0 +1,8 @@
+<?php
+/*2.Escribir la función calendario_mensual en calendario_ funciones.php, que reciba el año y el mes
+como argumentos, y cree una tabla con la representación de ese mes, como si fuera una página
+de un almanaque. En la primera fla aparecerá el nombre del mes, en la segunda los nombres de
+los días de la semana abreviados (L, M, X, J, V, S y D) y, en las siguientes, los números de los
+días*/
+
+echo "hola";
