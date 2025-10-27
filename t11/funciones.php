ef482d890f5b17e173a7d9009599f814eb68809f
BryanDZV <bryan.dweb@gmail.com>
2025-10-27 11:15:04 +0100

    cookies

--- t11/funciones.php
+++ t11/funciones.php
@@ -1,23 +1,21 @@
 <?php
-function crearTabla($columna, $fila, $ancho = 'auto', $color = 'red', $bordes = 1)
+function crearTabla($columna, $fila, $ancho = 'auto', $color = 'red', $bordes = 8)
 {
-    // Validate inputs to ensure they are numbers
     $columna = (int)$columna;
     $fila = (int)$fila;
     $bordes = (int)$bordes;
 
-    // Build inline style from parameters
-    $style = "width: {$ancho}px; border: {$bordes}px solid {$color}; border-collapse: collapse;";
-    $cellStyle = "border: {$bordes}px solid {$color}; padding: 5px;";
+    //$style = "width: {$ancho}px; border: {$bordes}px solid {$color}; border-collapse: collapse;";
+    //$cellStyle = "border: {$bordes}px solid {$color}; padding: 5px;";
 
-    $tabla = "<table style='{$style}'>";
-    $tabla .= "<thead><tr><th colspan='{$columna}' style='{$cellStyle}'>Tabla Generada</th></tr></thead>";
+    $tabla = "<table class='tabla'>";
+    $tabla .= "<thead><tr><th colspan='{$columna}' class='celda' style='{bordes}'>Tabla Generada</th></tr></thead>";
     $tabla .= "<tbody>";
 
     for ($i = 0; $i < $fila; $i++) {
         $tabla .= "<tr>";
         for ($j = 0; $j < $columna; $j++) {
-            $tabla .= "<td style='{$cellStyle}'>Fila " . ($i + 1) . ", Col " . ($j + 1) . "</td>";
+            $tabla .= "<td class='celda'>Fila " . ($i + 1) . ", Col " . ($j + 1) . "</td>";
         }
         $tabla .= "</tr>";
     }
---
+++ t12/ej1/vercookies.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("fecha", false, time() + 3600);
+var_dump($_COOKIE);
