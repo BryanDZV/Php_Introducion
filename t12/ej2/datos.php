828f20aa50aca831f6e3a149254a54c43e767f1c
Bryan <bryan.dweb@gmail.com>
2025-10-30 21:39:48 +0100

    cokies

--- t12/ej2/datos.php
+++ t12/ej2/datos.php
@@ -1,28 +1,38 @@
 <?php
 
-$datosPersonales = [
-    "nombre",
-    "direccion",
-    "fecha",
-    "telefono",
-    "email",
-];
 
+$traducciones = [
 
-$idiomas = [
-    $español = [
-        "nombre completo" => "nombre ",
-        "edad" => "edad",
-        "ciudad" => "Madrid"
+    // Versión en Español (ES)
+    'es' => [
+        "datos" => "Datos Personales",
+        "titulo" => "Formulario de Currículum",
+        "nombre" => "Nombre Completo",
+        "direccion" => "Dirección",
+        "fecha" => "Fecha de nacimiento",
+        "telefono" => "Teléfono",
+        "email" => "Email"
     ],
-    $ingles = [
-        "nombre" => "nombre",
-        "edad" => "edad",
-        "ciudad" => "Madrid"
+
+    // Versión en Inglés (EN)
+    'en' => [
+        "datos" => "Personal Data",
+        "titulo" => "Resume Form",
+        "nombre" => "Full Name",
+        "direccion" => "Address",
+        "fecha" => "Date of Birth",
+        "telefono" => "Phone",
+        "email" => "Email"
     ],
-    $aleman = [
-        "nombre" => "nombre",
-        "edad" => "edad",
-        "ciudad" => "Madrid"
+
+    // Versión en Alemán (DE)
+    'de' => [
+        "datos" => "Persönliche Daten",
+        "titulo" => "Lebenslauf-Formular",
+        "nombre" => "Vollständiger Name",
+        "direccion" => "Adresse",
+        "fecha" => "Geburtsdatum",
+        "telefono" => "Telefon",
+        "email" => "E-Mail"
     ]
 ];
---
+++ t12/ej3/funciones.php
@@ -0,0 +1,7 @@
+<?php
+
+
+function guardarNombre($nombre)
+{
+    return $nombre;
+}
