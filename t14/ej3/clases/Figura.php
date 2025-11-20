18303c3f7e1cb6ae4bc26ee882ae22332313f73c
Bryan <bryan.dweb@gmail.com>
2025-11-20 20:06:12 +0100

    dd

--- t14/ej3/clases/Figura.php
+++ t14/ej3/clases/Figura.php
@@ -7,15 +7,51 @@ abstract class  Figura
 
     protected $color = "green";
     protected $size = 0;
-
-    public function __construct($color, $size)
+    protected $nombre = "defecto";
+    public function __construct($color, $size, $nombre)
     {
         $this->color = $color;
         $this->size = $size;
+        $this->nombre = $nombre;
     }
-    abstract public function getClase();
-    public function __toString()
+
+    public function getColor()
     {
-        return "estas en la clase::-->" . $this->getClase();
+        return $this->color;
     }
+
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function setSize($size): void
+    {
+        $this->size = $size;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function __toString(): string
+    {
+        return $this->color . ", " . $this->size . ", " . $this->nombre . ",tu CLASE ES : " . $this->getClase();
+    }
+
+    abstract public function getClase();
+    abstract public function getPerimetro();
+
+    abstract public function getArea();
 }
--- t14/ej3/clases/Triangulo.php
+++ t14/ej3/clases/Triangulo.php
@@ -4,20 +4,83 @@ namespace Clases;
 
 class Triangulo extends Figura
 {
-    /*
-  
-     */
-    private $nombre = "triangulo";
+
     private $ladoA;
     private $ladoB;
     private $ladoC;
 
-    private $area;
-    private $perimetro;
+    public function __construct($nombre, $ladoA, $ladoB, $ladoC, $color, $size)
+    {
+        $this->ladoA = $ladoA;
+        $this->ladoB = $ladoB;
+        $this->ladoC = $ladoC;
+        parent::__construct($nombre, $color, $size);
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre): void
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getLadoA()
+    {
+        return $this->ladoA;
+    }
+
+    public function setLadoA($ladoA): void
+    {
+        $this->ladoA = $ladoA;
+    }
+
+    public function getLadoB()
+    {
+        return $this->ladoB;
+    }
+
+    public function setLadoB($ladoB): void
+    {
+        $this->ladoB = $ladoB;
+    }
+
+    public function getLadoC()
+    {
+        return $this->ladoC;
+    }
+
+    public function setLadoC($ladoC): void
+    {
+        $this->ladoC = $ladoC;
+    }
+
+    public function __toString(): string
+    {
+        return $this->nombre . ", " . $this->ladoA . ", " . $this->ladoB . ", " . $this->ladoC;
+    }
 
 
+
+    //implementaciones
     public function getClase()
     {
         return $this->nombre;
     }
+    public function getPerimetro()
+    {
+        return $this->ladoA + $this->ladoB + $this->ladoC;
+    }
+
+    public function getArea()
+    { //heron 3 lados conocidos
+        $s = $this->getPerimetro() / 2;
+        return sqrt($s * ($s - $this->ladoA) * ($s - $this->ladoB) * ($s - $this->ladoC));
+    }
+
+
+    //funciones propias
+
 }
