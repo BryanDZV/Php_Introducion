49ff5f3ecbf9cfff95bb584c07a88f9753d5810d
Bryan <bryan.dweb@gmail.com>
2025-11-21 02:19:35 +0100

    figuras

--- t14/ej3C/datos/datos.php
+++ t14/ej3C/datos/datos.php
@@ -19,21 +19,3 @@ $colores = [
   "pink"   => [255, 192, 203],
   "gray"   => [128, 128, 128]
 ];
-/*| Caso de uso                         | Mejor usar           |
-| ----------------------------------- | -------------------- |
-| Solo para formulario                | **Array PHP (.php)** |
-| Lo modificará el usuario o profesor | JSON                 |
-| Lo traerás con AJAX o JavaScript    | JSON                 |
-| Son datos estáticos y simples       | Array PHP            |
-| Proyecto grande y configurable      | JSON o Base de datos |
-*/
-/*| ¿Situación?                                  | ¿JSON o Array PHP? |
-| -------------------------------------------- | ------------------ |
-| Solo pintar formulario                       | ⭐ **Array PHP**    |
-| Los datos no cambian                         | ✔ Array PHP        |
-| Sería siempre igual                          | ✔ Array PHP        |
-| El profesor no te pide configuración externa | ✔ Array PHP        |
-| No lo modificará el usuario                  | ✔ Array PHP        |
-| Proyecto pequeño / educativo                 | ✔ Array PHP        |
-| Si se quisiera configurable desde fuera      | JSON               |
-*/
